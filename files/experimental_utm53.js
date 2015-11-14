21bb1d6fd7414a253a1ae60b43b1c5d82a6165c8
Minoru Akagi <akaginch@gmail.com>
2015-11-14 13:54:05 +0900

    add GSI tiles to experimental UTM53 project

--- files/experimental_utm53.js
+++ files/experimental_utm53.js
@@ -9,10 +9,15 @@ olapp.loadProject(new olapp.Project({
     maxZoom: 20,
     zoom: 8
   }),
-  plugins: ['source/naturalearth.js'],
+  plugins: ['source/naturalearth.js', 'source/gsitiles.js'],
   init: function (project) {
     // Natural Earth data
     var ne = new olapp.source.NaturalEarth;
     project.addLayer(ne.createLayer('cl'));       // Coastline
+
+    // GSI Tiles
+    var gsitiles = new olapp.source.GSITiles;
+    project.addLayer(gsitiles.createLayer('std'));                        // 標準地図
+    project.addLayer(gsitiles.createLayer('relief', {visible: false}));   // 色別標高図
   }
 }));
