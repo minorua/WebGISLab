0d5e35fbed9539e9547695e0ffa56d0b78794311
Minoru Akagi <akaginch@gmail.com>
2015-12-10 14:50:52 +0900

    Gist: URL input form

--- plugins/source/gist.js
+++ plugins/source/gist.js
@@ -21,26 +21,25 @@
 
   var html =
 '<li class="list-group-item">' +
-'  <button type="button" class="btn btn-primary">Add</button>' + layers[0].name +
+'  <label style="width: 100%;">Enter Gist URL:<br>' +
+'    <input type="text" style="width: 100%; font-weight: normal;" title="Gist URL (https://gist.github.com/....) or raw file URL (https://gist.githubusercontent.com/....)">' +
+'  </label>' +
+'  <div style="text-align: right; height: 18px;"><button type="button" class="btn btn-primary">Fetch</button></div>' +
 '</li>';
   layers[0].listItem = $(html);
 
   /* olapp.source.Gist */
   olapp.source.Gist = new olapp.Source('Gist', layers);
   olapp.source.Gist.createLayer = function (id, layerOptions) {
-    var url;
-    if (id != 'new') url = id;
-    else {
-      url = window.prompt('Input Raw Gist URL (https://gist.githubusercontent.com/...)');
-      if (!url) return null;
-      if (url.indexOf('https://gist.githubusercontent.com/') !== 0) {
-        alert('Invalid URL');
-        return null
-      }
+    var url = id;
+    if(url.indexOf('https://gist.githubusercontent.com/') !== 0) {
+      console.log('Invalid URL', url);
+      return null;
     }
+
     var filename = url.split('/').pop();
     var options = {
-      style: olapp.core.createStyleFunction(),
+      style: olapp.core.createStyleFunction(),   // TODO: store color in olapp.style
       title: filename
     };
     var layer = new ol.layer.Vector($.extend(options, layerOptions));
@@ -50,28 +49,70 @@
       if (source) {
         layer.setSource(source);
         layer.get('olapp').layer = url;
-        // TODO: store to local storage as a recently used Gist file
       }
       else {
-        bootbox.alert('Failed to create a layer from the Gist file.');
+        bootbox.alert('Failed to create a layer from the Gist file.<br>' + url);
         olapp.project.removeLayer(layer.get('id'));
       }
     }, function () {
-      bootbox.alert('Failed to download Gist file.');
+      bootbox.alert('Failed to download Gist file:<br>' + url);
       olapp.core.project.removeLayer(layer.get('id'));
     });
     return layer;
   };
 
   // event handler
-  layers[0].listItem.children('button').click(function () {
-    var layerOptions = {
-      olapp: {
-        source: sourceName
-      }
-    };
-    var layer = olapp.source.Gist.createLayer(layers[0].id, layerOptions);
-    if (layer) olapp.core.project.addLayer(layer);
+  layers[0].listItem.find('button').click(function () {
+    var url = layers[0].listItem.find('input').val();
+    if (!url) return;
+
+    var list = layers[0].listItem.parent();
+    function appendItem(url, filename, description) {
+      var html =
+'<li class="list-group-item">' +
+'  <button type="button" class="btn btn-default" title="Store this layer URL in local storage">' +
+'   <span class="glyphicon glyphicon-floppy-disk"></span>' +
+'  </button>' +
+'  <button type="button" class="btn btn-primary">Add</button>' + filename +
+'</li>';
+      $(html).appendTo(list).children('button').click(function () {
+        if ($(this).hasClass('btn-primary')) {  // Add
+          var layerOptions = {
+            olapp: {
+              source: sourceName
+            }
+            // TODO: set description property
+          };
+          var layer = olapp.source.Gist.createLayer(url, layerOptions);
+          if (layer) olapp.core.project.addLayer(layer);
+        }
+        else {    // TODO: Store URL in local storage
+          // glyphicon-floppy-saved
+          // glyphicon-floppy-remove
+        }
+      });
+    }
+
+    if (url.indexOf('https://gist.github.com/') === 0) {
+      // Get metadata of the Gist
+      var gistId = url.split('/')[4];
+      $.getJSON('https://api.github.com/gists/' + gistId).then(function (json) {
+        if (!json.files) return;
+        for (var file in json.files) {
+          var f = json.files[file];
+          appendItem(f.raw_url, f.filename, json.description);
+        }
+      }, function () {
+        alert('Failed to download Gist metadata.');
+      });
+    }
+    else if(url.indexOf('https://gist.githubusercontent.com/') === 0) {
+      appendItem(url, url.split('/').pop());
+    }
+    else {
+      alert('Invalid URL');
+      return;
+    }
   });
 
   // register this source
